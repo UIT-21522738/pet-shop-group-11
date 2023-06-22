6315773ef1bc70ff9eb6c1b1011c1e1f8259b5fa
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2023-06-22 20:14:02 +0700

    fix create nv

--- server/src/app/controllers/StaffController.js
+++ server/src/app/controllers/StaffController.js
@@ -44,7 +44,7 @@ class StaffController {
             body.creater = data.code;
         })
 
-        Staff.findOne({$or: {phoneNumber: req.body.phoneNumber, username: req.body.username}})
+        Staff.findOne({$or: [{phoneNumber: req.body.phoneNumber}, {username: req.body.username}]})
         .then(async data => {
             if (data) {
                 res.statusCode = 402;
